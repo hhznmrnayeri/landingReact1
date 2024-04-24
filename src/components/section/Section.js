463290d3a63614a69f2685e1b50d7463f041542f
Heydar <h_h_z_n@yahoo.com>
2024-04-25 00:14:03 +0330

    handling section component

--- src/components/section/Section.js
+++ src/components/section/Section.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
-export default function Section() {
+export default function Section(props) {
   return (
-    <div className="container mt-10"></div>
+    <div className="container mt-10 py-14">
+      <div className='grid grid-cols-1 md:grid-cols-12 gap-2 items-center'>
+        <div className={`bg-cover ${props.styleName}`}></div>
+      </div>
+    </div>
   )
 }
