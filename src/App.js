6d05aec1833a947b47a720d459d701a9285f7bb5
Heydar <h_h_z_n@yahoo.com>
2024-04-24 12:42:26 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl  text-blue-600">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
