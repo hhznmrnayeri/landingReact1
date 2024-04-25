b55b73c3a57e59ed7a52c74ad5d311308fac1d83
Heydar <h_h_z_n@yahoo.com>
2024-04-25 13:37:21 +0330

    import SectionTitle component to Marketing component

--- src/components/marketing/Marketing.js
+++ src/components/marketing/Marketing.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import SectionTitle from './../clients/section-title'
 
 export default function Marketing() {
   return (
-    <div></div>
+    <div className='mt-12'>
+      <SectionTitle title='Caring is the new marketing' caption="The Nexcent blog is the best place to read about the latest membership insights, trends and more. See who's joining the community, read about how our community are increasing their membership income and lot's more.​"></SectionTitle>
+      
+    </div>
   )
 }
